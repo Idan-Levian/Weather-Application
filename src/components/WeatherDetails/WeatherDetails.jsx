9f0ce1a78ab850457baf5cd0821e1f975ae83b2e
Idan Levian <eidan47@gmail.com>
2020-11-18 02:43:31 +0200

    feat-8 added weather details component

---
+++ src/components/WeatherDetails/WeatherDetails.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import * as S from './style';
+
+/* Import Components */
+import TransparentBox from '../TransparentBox';
+import Subtitle from '../Subtitle';
+
+const WeatherDetails = () => {
+  return (
+    <TransparentBox opacity='0.15' color='gray'>
+      <S.UL>
+        <S.LI>
+          <Subtitle text='6&deg;' />
+          <Subtitle text='Hight' />
+        </S.LI>
+        <S.LI>
+          <Subtitle text='3&deg;' />
+          <Subtitle text='Low' />
+        </S.LI>
+        <S.LI>
+          <Subtitle text='3.1mph' />
+          <Subtitle text='Wind' />
+        </S.LI>
+        <S.LI>
+          <Subtitle text='70%' />
+          <Subtitle text='Rain' />
+        </S.LI>
+        <S.LI>
+          <Subtitle text='6:40' />
+          <Subtitle text='Sunrise' />
+        </S.LI>
+        <S.LI>
+          <Subtitle text='7:19' />
+          <Subtitle text='Sunset' />
+        </S.LI>
+      </S.UL>
+    </TransparentBox>
+  );
+};
+
+export default WeatherDetails;
