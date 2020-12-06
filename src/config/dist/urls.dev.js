3d15dba3172d4252f825dccc799a5bd436124523
Idan Levian <eidan47@gmail.com>
2020-12-06 20:42:22 +0200

    fix problem - mixed-block

--- src/config/dist/urls.dev.js
+++ src/config/dist/urls.dev.js
@@ -1,20 +1,24 @@
-"use strict";
+'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
+Object.defineProperty(exports, '__esModule', {
+  value: true,
 });
-exports["default"] = void 0;
+exports['default'] = void 0;
 
 /* eslint-disable import/no-anonymous-default-export */
 var _default = {
   weather: {
-    base: 'http://api.openweathermap.org/data/2.5',
+    base: 'https://api.openweathermap.org/data/2.5',
     forecast: function forecast(query) {
-      return "/onecall?".concat(query, "&units=metric&appid=").concat(process.env.REACT_APP_API_KEY);
+      return '/onecall?'
+        .concat(query, '&units=metric&appid=')
+        .concat(process.env.REACT_APP_API_KEY);
     },
     current: function current(query) {
-      return "/weather?q=".concat(query, "&units=metric&appid=").concat(process.env.REACT_APP_API_KEY);
-    }
-  }
+      return '/weather?q='
+        .concat(query, '&units=metric&appid=')
+        .concat(process.env.REACT_APP_API_KEY);
+    },
+  },
 };
-exports["default"] = _default;
+exports['default'] = _default;
--- src/config/urls.js
+++ src/config/urls.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default {
   weather: {
-    base: 'http://api.openweathermap.org/data/2.5',
+    base: 'https://api.openweathermap.org/data/2.5',
     forecast: (query) =>
       `/onecall?${query}&units=metric&appid=${process.env.REACT_APP_API_KEY}`,
     current: (query) =>
