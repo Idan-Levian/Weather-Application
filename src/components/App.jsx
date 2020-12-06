aa1cb61e99effe8c496b7428db6073181f3e0593
Idan Levian <eidan47@gmail.com>
2020-12-06 16:32:04 +0200

    fix env problem

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { WeatherContextProvider } from '../contexts/weatherContext';
 import Homepage from '../views/Homepage';
 
+require('dotenv').config();
+
 const App = () => {
   return (
     <WeatherContextProvider>
